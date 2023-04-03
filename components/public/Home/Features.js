560edbd28f4afeddebcb076c2cc4276088e7f329
AliSiddique <alisiddique10@hotmail.com>
2023-04-03 13:50:14 +0100

    nice

--- components/public/Home/Features.js
+++ components/public/Home/Features.js
@@ -46,10 +46,9 @@ import {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
             <h2 className="text-base font-semibold leading-7 text-indigo-400">Everything you need</h2>
-            <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">No server? No problem.</p>
+            <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Need to be more productive</p>
             <p className="mt-6 text-lg leading-8 text-gray-300">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste
-              dolor cupiditate blanditiis.
+              Using the API&apns;s in Broca is a great way to get your app up and running quickly.
             </p>
           </div>
           <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base leading-7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-16">
